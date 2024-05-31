072545a65bdaacc765545dab47d7d31e49b27e63
maearon <manhng132@gmail.com>
2024-06-01 01:33:09 +0700

    admin template.tsx

---
+++ app/admin/page.tsx
@@ -0,0 +1,10 @@
+import Skeleton from 'react-loading-skeleton'
+
+export default function Admin() {
+  return (
+    <>
+    <Skeleton height={304} />
+    <Skeleton circle={true} height={60} width={60} />
+    </>
+  )
+}
---
+++ app/admin/template.tsx
@@ -0,0 +1,42 @@
+"use client";
+import Link from "next/link";
+
+import { usePathname } from "next/navigation";
+import { useState } from "react";
+
+const navLinks = [
+  { name: "Register", href: "/register" },
+  { name: "Login", href: "/login" },
+  { name: "Forgot Password", href: "/forgot-password" },
+];
+
+export default function AuthLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const pathname = usePathname();
+  const [input, setInput] = useState("");
+
+  return (
+    <div>
+      <div>
+        <input value={input} onChange={(e) => setInput(e.target.value)} />
+      </div>
+      {navLinks.map((link) => {
+        const isActive = pathname.startsWith(link.href);
+
+        return (
+          <Link
+            className={isActive ? "font-bold mr-4" : "text-blue-500 mr-4"}
+            href={link.href}
+            key={link.name}
+          >
+            {link.name}
+          </Link>
+        );
+      })}
+      {children}
+    </div>
+  );
+}
